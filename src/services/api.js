0bff702e6439ada44275ba8606c6bfb64763e6a4
Italo Leandro <leandroitalo433@gmail.com>
2020-05-10 11:03:29 -0300

    Exibindo alerta ao nao encontrar usuario

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,18 @@
 import axios from 'axios';
+import { Alert } from 'react-native';
 
 const api = axios.create({
   baseURL: 'https://api.github.com',
 });
+api.interceptors.response.use(
+  (response) => response,
+  (err) => {
+    if (err.response.status === 404) {
+      Alert.alert('Usuário não encontrado');
+    }
+
+    return Promise.reject(err);
+  }
+);
 
 export default api;
