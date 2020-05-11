824d5d2353557ae9e0671653e3588b9d94b24d19
Italo Leandro <leandroitalo433@gmail.com>
2020-05-10 21:28:23 -0300

    Configuraçoes necessarias para execucao do mobx

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
+import { Provider } from 'mobx-react';
 
 import './config/ReactotronConfig';
-
+import Stores from './stores/RootStores';
 import Routes from './routes';
 
 const App = () => {
   return (
-    <>
+    <Provider {...Stores}>
       <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
       <Routes />
-    </>
+    </Provider>
   );
 };
 
