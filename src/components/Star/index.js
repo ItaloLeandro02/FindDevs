1447eb162f07606a8115dab3bb5047cf5a67d5a8
Italo Leandro <leandroitalo433@gmail.com>
2020-05-13 01:27:57 -0300

    Component Star adicionado navegaçao para a rota do repositorio

--- src/components/Star/index.js
+++ src/components/Star/index.js
@@ -3,12 +3,14 @@ import PropTypes from 'prop-types';
 
 import { Author, Container, Info, OwnerAvatar, Title } from './styles';
 
-const Star = ({ star }) => {
+const Star = ({ navigation, star }) => {
   return (
     <Container>
       <OwnerAvatar source={{ uri: star.owner.avatar_url }} />
       <Info>
-        <Title>{star.name}</Title>
+        <Title onPress={() => navigation.navigate('Repository', { star })}>
+          {star.name}
+        </Title>
         <Author>{star.owner.login}</Author>
       </Info>
     </Container>
@@ -16,6 +18,10 @@ const Star = ({ star }) => {
 };
 
 Star.propTypes = {
+  navigation: PropTypes.shape({
+    navigate: PropTypes.func,
+  }).isRequired,
+
   star: PropTypes.shape({
     owner: PropTypes.shape({
       avatar_url: PropTypes.string,
